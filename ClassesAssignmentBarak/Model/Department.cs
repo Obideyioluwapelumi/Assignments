e396fbee4631e8adc554b8c0b655d8f03c9aa405
Obideyi Oluwapelumi <115759643+Obideyioluwapelumi@users.noreply.github.com>
2024-09-10 13:31:40 +0100

    Add files via upload

---
+++ ClassesAssignmentBarak/Model/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAssignmentBarak.Model
+{
+    internal class Department
+    {
+        public Guid Id { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentCode { get; set; }  // CSC, AGP
+        public string DepertmentFaculty { get; set; }
+        public string HeadOfDepartment { get; set; }
+        public bool DepartmentalBus { get; set; }
+        public string DepartmentalEmail { get; set; }
+        public string DepartmentalContactLine { get; set; }
+        public string DepartmentWebsite { get; set; }
+        public int NumberOfGraduate { get; set; }
+
+    }
+}
