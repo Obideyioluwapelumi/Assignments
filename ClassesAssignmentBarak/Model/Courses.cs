cdf87ab6c6bb8aa36214a51f9f0e4819e8115023
Obideyi Oluwapelumi <115759643+Obideyioluwapelumi@users.noreply.github.com>
2024-09-10 09:59:55 +0100

    Add files via upload

---
+++ ClassesAssignmentBarak/Model/Courses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAssignmentBarak.Model
+{
+    internal class Courses
+    {
+        public Guid Id { get; set; }
+        public string CourseName { get; set; }      // Intro to Organic Chem
+        public string CourseCode { get; set; }      // CHEM108
+        public int CourseLevel { get; set; }
+        public int CourseUnit { get; set; }      
+        public string CourseDescription { get; set; }
+        public string CourseCreditUnit { get; set; }
+        public string CourseLectureHours { get; set; }
+        public string CoursePrerequisite { get; set; }
+        public string CourseDepartment { get; set; }
+        public string CourseFaculty { get; set; }
+        public int SemesterOfferingCourse { get; set; }
+        public string CourseAssessmentMethod { get; set; }      // Project,Exams,  
+    }
+}
