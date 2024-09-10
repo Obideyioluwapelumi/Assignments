7deda31694d8cdf3b140a3bf23338a93132bef3d
Obideyi Oluwapelumi <obideyioluwapelumi@gmail.com>
2024-09-10 14:48:52 +0100

    Commit Added

--- ClassesAssignmentBarak/Model/Lecturer.cs
+++ ClassesAssignmentBarak/Model/Lecturer.cs
@@ -9,22 +9,39 @@ namespace ClassesAssignmentBarak.Model
     internal class Lecturer
     {
         public Guid Id { get; set; }
+
         public int LecturerEmployeeID { get; set; }
+
         public string LecturerFirstName { get; set; }
+
         public string LecturerLastName { get; set; }
+
         public string LecturerAcademicTitle { get; set; }   //Dr., Prof
+
         public int LecturerYearOfExperience { get; set; }
+
         public string LecturerAreaOfExpertise { get; set; }
+
         public int LecturerNumberOfPublication { get; set; }
+
         public int LecturerNumberOfCitation { get; set; }
+
         public string LecturerDepartment { get; set; }
+
         public string LecturerFaculty { get; set; }
+
         public string LecturerCourseTaken { get; set; }
+
         public string LecturerEmploymentStatus { get; set; }
+
         public DateTime LecturerDateOfHire { get; set; }
+
         public string LecturerGender { get; set; }
+
         public DateTime LecturerDateOfBirth { get; set; }
+
         public string LecturerNationality { get; set; }
+
         public string LecturerResidentialAddress { get; set; }
     }
 }
