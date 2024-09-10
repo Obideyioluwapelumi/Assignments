8a9e27f47ce1a24ff27852130c5cced1a641e650
Obideyi Oluwapelumi <115759643+Obideyioluwapelumi@users.noreply.github.com>
2024-09-10 13:33:52 +0100

    Add files via upload

---
+++ ClassesAssignmentBarak/Model/Faculty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAssignmentBarak.Model
+{
+    internal class Faculty
+    {
+        public Guid Id { get; set; }
+        public int FacultyId { get; set; }
+        public string FacultyName { get; set; }
+        public DateTime DateOfEstablishment { get; set; }
+        public int NumberOfDepartments { get; set; }
+        public string LocationOfFaculty {  get; set; }
+        public string DeanOfFaculty { get; set; }
+        public int FacultyContactLine { get; set; }
+        public string FacultyEmail { get; set; }
+        public string FacultyWebsite { get; set; }
+        public int NumberOfProfessors  { get; set; }
+        public bool FacultyBus { get; set; }
+    }
+}
