c1a3fde438b3da5986b76091bd418b0feda8c280
Obideyi Oluwapelumi <obideyioluwapelumi@gmail.com>
2024-09-10 16:00:01 +0100

    Commit Added

--- ClassesAssignmentBarak/Program.cs
+++ ClassesAssignmentBarak/Program.cs
@@ -12,6 +12,23 @@ class Prgram
             new Faculty
             {
                 Id = Guid.NewGuid(),
+                FacultyId =      FG-IB-SCI-101           // Starting with the University ID code FGIB
+                FacultyName = "Faculty of Sciences"
+                DeanOfFaculty = "Professor S.K Mosafejo"
+            }
+
+            {
+                Id = Guid.NewGuid(),
+                FacultyId = FG-IB-ENG-102
+                FacultyName = "Faculty of Engineering"
+                DeanOfFaculty = "Professor L.S Konibaje"
+            }
+
+            {
+                Id = Guid.NewGuid(),
+                FacultyId = FG-IB-ART-103
+                FacultyName = "Faculty of Art"
+                DeanOfFaculty = "Professor M.O Enioriyo"
             }
         }
 
@@ -51,12 +68,7 @@ class Prgram
 
     }
 
-
-
-
-    static void Guid GenerateRandomId()
-            Guid.NewGuid();
-        
-        Console.WriteLine(
+    
+        Console.WriteLine();
     }
 }
